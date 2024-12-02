e907deb6b25a577fffba329844e09539c60fde20
sajones100 <143047969+sajones100@users.noreply.github.com>
2024-12-02 15:37:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const data = {
     title: "Welcome to CS3210 Template Engine Lab",
-    content: "This is a simple template engine demonstration.",
-    author: "Dr. Red",
+    content: "This is a template.",
+    author: "Seth",
 };
+
+function renderTemplate(template, data) {
+    for (const key in data) {
+        const regex = new RegExp('{{' + key + '}}', 'g');
+        template = template.replace(regex, data[key]);
+    }
+    return template;
+}
+
+const template = `
+    <h3>{{title}}</h3>
+    <p>{{content}}</p>
+    <p>Author: {{author}}</p>
+`;
+
+const app = document.getElementById('app');
+app.innerHTML = renderTemplate(template, data);
+
