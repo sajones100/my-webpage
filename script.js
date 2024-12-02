6429094903e3afc3669d762dfb9fdb650ca368dd
sajones100 <143047969+sajones100@users.noreply.github.com>
2024-12-02 15:39:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const data = {
-    title: "Welcome to CS3210 Template Engine Lab",
-    content: "This is a template.",
-    author: "Seth",
-};
-
-function renderTemplate(template, data) {
-    for (const key in data) {
-        const regex = new RegExp('{{' + key + '}}', 'g');
-        template = template.replace(regex, data[key]);
-    }
-    return template;
-}
-
-const template = `
-    <h3>{{title}}</h3>
-    <p>{{content}}</p>
-    <p>Author: {{author}}</p>
+// Define a template using Mustache.js
+var template = `
+    <ul>
+        {{#data}}
+        <li>Name: {{name}}, Age: {{age}}, City: {{city}}</li>
+        {{/data}}
+    </ul>
 `;
 
-const app = document.getElementById('app');
-app.innerHTML = renderTemplate(template, data);
+// Get the 'output' div element
+var output = document.getElementById('output');
 
+// Fetch JSON data from the file
+fetch('data.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // Render the data using the template
+        var rendered = Mustache.render(template, { data: data });
+        output.innerHTML = rendered;
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
+    });
